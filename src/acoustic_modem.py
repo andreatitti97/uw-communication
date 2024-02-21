00db8c97bf017be48e5733003f382b589c764209
andreatitti97 <andrea.tiranti97@gmail.com>
2024-02-21 16:25:41 +0100

    Implemented the TDMA protocol

--- src/acoustic_modem.py
+++ src/acoustic_modem.py
@@ -32,7 +32,6 @@ plot_path = os.path.abspath('/home/andrea/Desktop/ros_simulation_ws/src/ipp_pkg/
 
 # Initi Global Variables 
 m_rx = [0,0,0,0]
-m_tx_local = [0,0,0,0]
 
 def sig(x,d):
     
@@ -40,7 +39,7 @@ def sig(x,d):
     gamma = d
     return 1/(1+np.e**(alpha*(gamma-x)))
 
-def run_simulation():
+def run_simulation(pub_rx_meas,auvID):
 
     """Simulate the sensor platform and the moving target
     Input:  target : target initial state
@@ -51,10 +50,9 @@ def run_simulation():
             f : choosen geometry
             meas : initial s state
     """
-    global count1, m_tx_local, meas_rx, t_pose
+    global count1, m_rx, auv_xy
 
     Hz = 1/(config.TIME_STEP) #NB: different from sampling rate for move things, this is ros rate
-    Hz = 1
     rate = rospy.Rate(Hz)
 
     # Init time variables and counters and lists
@@ -65,60 +63,64 @@ def run_simulation():
     meas_table = []
     old_m = [0,0,0,0]
     epsi = 0.01
-    time.sleep(5000)
+    
     ## SIMULATION LOOP ############################################################################################################
     while not rospy.is_shutdown():
 
-
-
-        #TODO check if the NOT LOCAL measurements has alredy been processed.
+        #TODO check if the measurements has alredy been processed.
         if sum(np.abs(m_rx[0:3]))-sum(np.abs(old_m[0:3]))>epsi:
-            m_rx.append(m_tx_local[2])
-            m_rx.append(m_tx_local[3])
+            #print(sum(np.abs(m_rx[0:3]))-sum(np.abs(old_m[0:3])))
+            #rospy.loginfo(str(auvID)+'PROCESSING A NEW MEASUREMENT')
+            m_rx.append(auv_xy[0])
+            m_rx.append(auv_xy[1])
             meas_table.append(m_rx)
+            delay.append(0)
+            
+        for i in range(len(meas_table)):
+            measure = meas_table[i]
+            delay[i] += dt#TODO: BUG HERE
+            d = np.sqrt((measure[5]-measure[3])**2+(measure[4]-measure[2])**2)
+            prob = sig(d,d)
+            if delay[i] >= d*(1/c)*10:
+                pub_rx_meas.publish(np.array(m_rx,dtype=np.float32))
+                '''if (np.random.random() <= prob):
+                    #msg received
+                    rcvd_pkt +=1
 
-            for i in range(len(meas_table)):
-                delay[i] += dt
-                d = np.sqrt((meas_table[i,5]-meas_table[i,3])**2+(meas_table[4]-meas_table[i,2])**2)
-                prob = sig(d,d)
-                if delay[i] >= d*(1/c):
-                    if (np.random.random() <= prob):
-                        #msg received
-                        rcvd_pkt +=1
-                        #if i+1 != auvID: #publish it
-                        #    pub_rx_meas[i].publish(m_rx)
-                            
-                    else:#msg lost
-                        lost_pkt += 1
-                        print('LOST PACKETT')
+                    #for j in range(len(pub_rx_meas)):
+                        
+                    pub_rx_meas.publish(np.array(m_rx,dtype=np.float32))
+                        
+                else:#msg lost
+                    lost_pkt += 1
+                    rospy.logwarn('AUV'+str(auvID)+'Lost a Packet')'''
                     
-                    # REMOVE THE MEASUREMENT FROM THE TABLE 
-                    meas_table.pop(i)
-                    delay[i] = 0
+                
+                # REMOVE THE MEASUREMENT FROM THE TABLE 
+                meas_table.pop(i)
+                delay.pop(i)
         
         old_m = m_rx
-
         t += dt
         count1 += 1 
-
         rate.sleep()
-
-def callback(data):
-    
-    global m_tx_local
-    m_tx_local = data.data
 
 def callback2(data):
     global m_rx
     
-    m_rx = data.data
+    tmp = data.data
+    m_rx = [tmp[0],tmp[1],tmp[2],tmp[3]]
 
-    
+def callbackAuvState(data):
+    global auv_xy
+    tmp = data.data
+    auv_xy = [tmp[0],tmp[1]]
+
 def listener(auvID,auvNum):
 
-    #rospy.Subscriber('tx_meas_local', numpy_msg(Floats), callback)
+    rospy.Subscriber('vehicle_state_'+str(auvID), numpy_msg(Floats), callbackAuvState)
     for i in range(auvNum):
-        #if i+1 != auvID:
+        
         rospy.Subscriber('/'+str(i+1)+'/tx_meas', numpy_msg(Floats), callback2)  
     
 def main():
@@ -129,6 +131,7 @@ def main():
     # Get AUV ID and number of vehicles.
     auvID = rospy.get_param(params_path+'/auvID')
     auvNum = rospy.get_param(params_path+'/auvNum')
+ 
     # Node Init
     rospy.init_node('acoustic_modem'+str(auvID))
     # Publishers init
@@ -140,11 +143,12 @@ def main():
             tmp = rospy.Publisher('/'+str(i+1)+'/uw_channel', numpy_msg(Floats), queue_size=100)
             pub_rx_meas.append(tmp)'''
     for i in range(auvNum):
-        pub_rx_meas = rospy.Publisher('/'+str(i+1)+'/rx_meas', numpy_msg(Floats), queue_size=100)
-
+        tmp = rospy.Publisher('/'+str(i+1)+'/rx_meas', numpy_msg(Floats), queue_size=100)
+        pub_rx_meas.append(tmp)
+    pub_rx_meas = rospy.Publisher('/'+str(auvID)+'/rx_meas', numpy_msg(Floats), queue_size=100)
     # Start listener and simulation
     listener(auvID,auvNum)
-    run_simulation()
+    run_simulation(pub_rx_meas,auvID)
 
     rospy.spin()
 
