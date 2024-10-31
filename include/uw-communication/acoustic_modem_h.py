92d9d52414cdefa3c57ecc1c9908db597a9b55e7
andreatitti97 <andrea.tiranti97@gmail.com>
2024-10-31 16:10:41 +0100

    Updated the simulator to the multi-target case + Improved code

--- include/uw-communication/acoustic_modem_h.py
+++ include/uw-communication/acoustic_modem_h.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import os
 import importlib.util, pathlib
+import random
+import numpy as np
 
 # Import Costum classes
 class_path = pathlib.Path(__file__).parent.resolve()
@@ -16,3 +18,24 @@ spec.loader.exec_module(sensor)
 spec = importlib.util.spec_from_file_location("module.tracker", class_path+'/tracker.py')
 tracker = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(tracker)
+
+
+def simulatePktDelivery(pdr, stddev=0.001):
+    """
+    Simulates packet transmission between two nodes based on packet delivery ratio.
+    
+    Parameters:
+    - pdr (float): Mean packet delivery ratio as a percentage (0-100).
+    - stddev (float): Standard deviation of the packet delivery ratio, default is 5%.
+    
+    Returns:
+    - bool: True for a successful transmission, False for a failed transmission.
+    """
+    # Generate a packet delivery success probability using a Gaussian distribution
+    success_probability = random.gauss(pdr, stddev)
+    
+    # Clamp the probability to be between 0 and 100
+    success_probability = max(0, min(100, success_probability))
+    
+    # Generate a random value between 0 and 100 to decide success or failure
+    return np.random.uniform(0, 100) < success_probability
--- src/acoustic_modem_MTT.py
+++ src/acoustic_modem_MTT.py
@@ -3,31 +3,28 @@
 import os, importlib.util, pathlib
 # Import numpy modules
 import numpy as np
+import random
 #Import ROS modules
 import rospy
 from rospy_tutorials.msg import Floats
 from rospy.numpy_msg import numpy_msg
 from uwmsn_msgs.msg import Matrix
 
-# Load the header file as a Python module 
+# Load the h file as a Python module 
 pkg_directory = os.path.dirname(os.path.dirname(pathlib.Path(__file__).parent.resolve()))
 header_file = pkg_directory+'/uw-communication'+'/include'+'/uw-communication'
 log_path = pkg_directory+'/logs'
 
 spec = importlib.util.spec_from_file_location("module.header", header_file+'/acoustic_modem_h.py')
-header = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(header)
+h = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(h)
 
+
+# VELOCITÀ DI COMUNICATION FISSA = 480 bps!
 # Init Global Variables for callbacks
-setRx = [[0],[0],[0],[0],[0]]
 rcvd_pkt, lost_pkt = 0, 0
-pi_bar = [[], [], [], []]
-
-def sig(x):
-    
-    alpha = header.config.alpha
-    gamma = header.config.gamma
-    return 1/(1+np.e**(alpha*(gamma-x)))
+pi_bar = [[] for _ in range(len(h.config.AUV_XY))]
+setRx = [[0],[0],[0],[0],[0]]
 
 def run_acoustic_modem(pub_rx_meas,pub_intent ,auvID, auvNum):
 
@@ -41,7 +38,7 @@ def run_acoustic_modem(pub_rx_meas,pub_intent ,auvID, auvNum):
     global count1, setRx, auv_xy, pi_bar, rcvd_pkt, lost_pkt
 
     # Rospy sim params
-    Hz = 1/(header.config.TIME_STEP) #NB: different from sampling rate for move things, this is ros rate   
+    Hz = 1/(h.config.TIME_STEP) #NB: different from sampling rate for move things, this is ros rate   
     rate = rospy.Rate(Hz)
 
     # Init variables, lists, bool
@@ -51,10 +48,11 @@ def run_acoustic_modem(pub_rx_meas,pub_intent ,auvID, auvNum):
     update_buff = False
     
     # Load simulation parameters from config file
-    t_scaler = header.config.TIME_SCALER
-    dt = header.config.TIME_STEP*t_scaler
-    c = header.config.c #(m/s)
-    buffLen = header.config.buffLen
+    t_scaler = h.config.TIME_SCALER
+    dt = h.config.TIME_STEP*t_scaler
+    c = h.config.c #(m/s)
+    PDR = h.config.PDR
+    buffLen = 30
     for i in range(buffLen):
         delay.append(0)
         buffer.append([0])#create a buffer 
@@ -66,7 +64,7 @@ def run_acoustic_modem(pub_rx_meas,pub_intent ,auvID, auvNum):
         measRx = setRx[0]
         
         if measRx[0] - measRxOld[0] > 1:#check if a new set of meas has been overwritten
-
+            
             # Check where to put the measurement in the buffer
             idx = buffer.index([0])#put the measurement at the index with
             #convert received numpy array to list
@@ -76,7 +74,7 @@ def run_acoustic_modem(pub_rx_meas,pub_intent ,auvID, auvNum):
                 tmp_setRx.append([tmp[0],tmp[1],tmp[2],tmp[3],tmp[4]])
 
             buffer[idx] = tmp_setRx
-                      
+
         for i in range(len(buffer[0:idx])):
             
             setTx = buffer[i]
@@ -85,21 +83,20 @@ def run_acoustic_modem(pub_rx_meas,pub_intent ,auvID, auvNum):
             
             if measure != 0:
                 d = np.sqrt((auv_xy[1]-measure[3])**2+(auv_xy[0]-measure[2])**2)
-                prob = sig(d)
 
-                if delay[i] >= d*(1/c)*2 or d < 10:
+                if delay[i] >= (d*(1/c)*2) or d < 10:#latencies according to delay
 
                     idx_rmv.append(i)
-                    update_buff = True
-                    
-                    if (np.random.random() <= prob) or d < 10:
+                    update_buff = True                    
+                    if h.simulatePktDelivery(PDR):
                         #msg received
                         rcvd_pkt += 1
-
                         setTx = np.array(setTx,dtype=np.float32)
                         rows, cols = setTx.shape
+                        # First Traffic - consensus (distributed estimation is better)
                         pub_rx_meas.publish(Matrix(data=setTx.flatten().tolist(), rows=rows, cols=cols))
                         tmp1 = pi_bar[auvID-1]
+                        # Second Traffic - Cooperation and Navigation (policy of intent)
                         pub_intent.publish(np.array(tmp1,dtype=np.float32))
                             
                     else:#msg lost
@@ -118,7 +115,7 @@ def run_acoustic_modem(pub_rx_meas,pub_intent ,auvID, auvNum):
                     idx = buffer.index([0])
                     update_buff = False
 
-        if int(t) == (header.config.TIME_DURATION-1):
+        if int(t) == (h.config.TIME_DURATION-1):
             rospy.on_shutdown(shutdown_cllbk)
             rospy.signal_shutdown('Simulation time limit reached')
 
@@ -147,33 +144,35 @@ def callbackMeasTx(data):
     global setRx
     setRx = np.array(data.data).reshape(data.rows, data.cols)
 
-def callback1(data):
-    global pi_bar
-    tmp = data.data
-    pi_bar[0] = tmp
+def create_callback(index):
+    def callback(data):
+        global pi_bar
+        pi_bar[index] = data.data
+    return callback
 
-def callback2(data):
-    global pi_bar
-    tmp = data.data
-    pi_bar[1] = tmp
+def listener(auvID,auvNum,netTopology):
 
-def callback3(data):
-    global pi_bar
-    tmp = data.data
-    pi_bar[2] = tmp
-
-def callback4(data):
-    global pi_bar
-    tmp = data.data
-    pi_bar[3] = tmp
-
-def listener(auvID,auvNum):
-
-    rospy.Subscriber('vehicle_state_'+str(auvID), numpy_msg(Floats), callbackAuvState)
-    callbackCtrlPolicy = [callback1,callback2,callback3,callback4]
+    """
+    Sets up subscribers only for neighboring agents.
+    
+    Parameters:
+    - auvID (int): ID of the current AUV (agent).
+    - auvNum (int): Total number of AUVs.
+    - netTopology (list): List of vectors defining network topology.
+    """
+    # Subscribe to each neighbor's topic
+    print('AUV ID',auvID)
+    print('network topologuy',netTopology)
     for i in range(auvNum):
-        rospy.Subscriber('/'+str(i+1)+'/tx_meas', Matrix, callbackMeasTx) 
-        rospy.Subscriber('/'+str(i+1)+'/tx_ctrl_policy',numpy_msg(Floats), callbackCtrlPolicy[i]) 
+        if auvID != i+1:
+            if i+1 in netTopology:
+                print('AUV ID',auvID)
+                print('Number of Neighbours:',len(netTopology))
+                
+                rospy.Subscriber('/'+str(i+1)+'/tx_meas', Matrix, callbackMeasTx) 
+        
+        rospy.Subscriber('/'+str(i+1)+'/tx_ctrl_policy', numpy_msg(Floats), create_callback(i))
+    rospy.Subscriber('vehicle_state_'+str(auvID), numpy_msg(Floats), callbackAuvState)
 
 def main():
 
@@ -181,11 +180,12 @@ def main():
     namespace = rospy.get_namespace()
     params_path = namespace+'acoustic_modem'
 
-    # Get AUV ID and number of vehicles.
+    # Get AUV ID and number of vehicles and network topology
     global auvID, pi_bar
     auvID = rospy.get_param(params_path+'/auvID')
     auvNum = rospy.get_param(params_path+'/auvNum')
-     
+    netTopology = h.config.netTopology#should be added as ros param
+
     # Node Init
     rospy.init_node('acoustic_modem'+str(auvID)) #log_level=rospy.DEBUG
 
@@ -194,7 +194,7 @@ def main():
     pub_rx_ctrl_policy = rospy.Publisher('/'+str(auvID)+'/rx_ctrl_policy', numpy_msg(Floats), queue_size=1000)
     
     # Start simulation
-    listener(auvID,auvNum)
+    listener(auvID,auvNum,netTopology[auvID-1])
     run_acoustic_modem(pub_rx_meas, pub_rx_ctrl_policy, auvID,auvNum)
     rospy.on_shutdown(shutdown_cllbk)
     rospy.spin()
